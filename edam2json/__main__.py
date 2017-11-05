a552982152c9f2023473b396747059fc401d40c5
Hervé  MENAGER <herve.menager@pasteur.fr>
2017-11-05 22:51:19 +0100

    enable generation of JSON-LD or bio.tools "extended" json

--- edam2json/__main__.py
+++ edam2json/__main__.py
@@ -31,28 +31,53 @@ def listify(obj, key):
     value = obj.get(key,[])
     return value if isinstance(value, list) else [value]
 
-def process_node(node):
-    biotools_node = {'text': node['rdfs:label'],
-                     'data': {'uri': node['@id']},
+def process_node(node, json_ld):
+    biotools_node = {'data': {'uri': node['@id']},
                      'children':[]}
+    text = node.get('rdfs:label')
+    if text is not None:
+        node['text'] = text
     biotools_node['exact_synonyms'] = node.get('oboInOwl:hasExactSynonym',[])
     biotools_node['narrow_synonyms'] = node.get('oboInOwl:hasNarrowSynonym',[])
+    biotools_node['consider'] = node.get('oboInOwl:consider',[])
+    biotools_node['replacedBy'] = node.get('oboInOwl:replacedBy',[])
+    description = node.get('oboInOwl:hasDefinition')
+    if description is not None:
+        biotools_node['definition'] = description
     for term in json_ld['@graph']:
         subclass_of = listify(term,'rdfs:subClassOf')
         if {'@id':node['@id']} in subclass_of:
-            biotools_node['children'].append(process_node(term))
+            biotools_node['children'].append(process_node(term, json_ld))
     return biotools_node
+
+def print_jsonld(args):
+    json_ld = get_json_ld(args.file)
+    print(json.dumps(json_ld, sort_keys=True,
+                  indent=4, separators=(',', ': ')))
+
+def print_biotools(args):
+    json_ld = get_json_ld(args.file) 
+    root = args.root
+    try:
+        root_node = [term for term in json_ld['@graph'] if term['@id']==root][0]
+    except IndexError:
+        print('Cannot find term "' + root +'" in EDAM ontology')
+        return
+    biotools_node = process_node(root_node, json_ld)
+    print(json.dumps(biotools_node, sort_keys=True,
+                  indent=4, separators=(',', ': ')))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                  description='EDAM file import tool')
     parser.add_argument('file',
                         help="path to the EDAM file")
+    subparsers = parser.add_subparsers(dest='action')
+    subparsers.required = True
+    parser_jsonld = subparsers.add_parser('jsonld')
+    parser_jsonld.set_defaults(func=print_jsonld)
+    parser_biotools = subparsers.add_parser('biotools')
+    parser_biotools.add_argument('--root',default='http://edamontology.org/topic_0003')
+    parser_biotools.set_defaults(func=print_biotools)
     args = parser.parse_args()
-
-    json_ld = get_json_ld(args.file)
-    root = 'topic_0003'
-    root_node = [term for term in json_ld['@graph'] if term['@id']=='http://edamontology.org/'+root][0]
-    biotools_node = process_node(root_node)
-    print(json.dumps(biotools_node, sort_keys=True,
-              indent=4, separators=(',', ': ')))
+    args.func(args)
